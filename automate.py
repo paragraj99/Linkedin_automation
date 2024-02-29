f9c545bd284e30c17a0486e621b990a33e111808
paragraj waghmare <paragrajwaghmare@gmail.com>
2024-02-29 16:36:09 +0530

    correction

--- automate.py
+++ automate.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
 import pandas as pd
 from selenium.webdriver.chrome.options import Options
